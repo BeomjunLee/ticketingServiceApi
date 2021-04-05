dbaf3518d648a518aeb2a89496a91aae7e8dcf55
leebeomjun <qjawnswkd0717@gmail.com>
2021-04-06 01:44:29 +0900

    페치조인 성능 테스트

---
+++ store-ticketing-service/src/test/java/com/hoseo/hackathon/storeticketingservice/service/AdminServiceTest.java
@@ -0,0 +1,39 @@
+package com.hoseo.hackathon.storeticketingservice.service;
+
+import com.hoseo.hackathon.storeticketingservice.domain.dto.MemberListDto;
+import com.hoseo.hackathon.storeticketingservice.domain.status.MemberStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Description;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+class AdminServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    AdminService adminService;
+    @Autowired
+    StoreService storeService;
+
+    @Test
+    @DisplayName("전체회원 목록 확인")
+    void findMembers() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<MemberListDto> members = adminService.findMembers(pageable, MemberStatus.VALID);
+        List<MemberListDto> content = members.getContent();
+        for (MemberListDto dto : content) {
+            System.out.println("dto = " + dto);
+        }
+    }
+
+}
