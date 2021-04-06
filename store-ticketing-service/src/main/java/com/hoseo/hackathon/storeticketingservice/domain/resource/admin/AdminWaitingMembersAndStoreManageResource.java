0dc083a992fdbb532be7fef581ccdf69dc981ce9
leebeomjun <qjawnswkd0717@gmail.com>
2021-04-07 01:41:53 +0900

    uri 수정

--- store-ticketing-service/src/main/java/com/hoseo/hackathon/storeticketingservice/domain/resource/admin/AdminWaitingMembersAndStoreManageResource.java
+++ store-ticketing-service/src/main/java/com/hoseo/hackathon/storeticketingservice/domain/resource/admin/AdminWaitingMembersAndStoreManageResource.java
@@ -13,10 +13,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 public class AdminWaitingMembersAndStoreManageResource extends EntityModel<StoreManageDto> { //가게 관리 메서드에 사용
     public AdminWaitingMembersAndStoreManageResource(StoreManageDto dto, Link... links){
         super(dto, links);
-        add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("hold").withRel("보류된 번호표 리스트 관리"));
-        add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("status").withRel("번호표 OPEN"));
-        add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("status").withRel("번호표 CLOSE"));
-        add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("errors").withRel("오류 접수"));
+        add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("holding").withRel("보류된 번호표 리스트 관리"));
+        add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("open-status").withRel("번호표 OPEN"));
+        add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("close-status").withRel("번호표 CLOSE"));
+        add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("apply-errors").withRel("오류 접수"));
         add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("--").withRel("공지사항 수정"));
         add(linkTo(ApiAdminController.class).slash("stores").slash(dto.getStore_id()).slash("--").withRel("한사람당 대기시간 수정"));
     }
