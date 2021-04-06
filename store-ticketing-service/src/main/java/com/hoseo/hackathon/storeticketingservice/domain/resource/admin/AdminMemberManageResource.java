964eb6d26fa0bbf7ed903fd5175c823923bc6eb6
leebeomjun <qjawnswkd0717@gmail.com>
2021-04-07 01:41:25 +0900

    uri 수정

--- store-ticketing-service/src/main/java/com/hoseo/hackathon/storeticketingservice/domain/resource/admin/AdminMemberManageResource.java
+++ store-ticketing-service/src/main/java/com/hoseo/hackathon/storeticketingservice/domain/resource/admin/AdminMemberManageResource.java
@@ -14,8 +14,8 @@ public class AdminMemberManageResource extends EntityModel<AdminMemberManageDto>
     public AdminMemberManageResource(AdminMemberManageDto dto, Link... links) {
         super(dto, links);
         add(linkTo(ApiStoreController.class).slash("members").withSelfRel());
-        add(linkTo(ApiStoreController.class).slash("members/auto").withRel("탈퇴후 7일지난 회원 영구삭제"));
-        add(linkTo(ApiStoreController.class).slash("members/search").withRel("검색"));
+        add(linkTo(ApiStoreController.class).slash("delete-members").withRel("탈퇴후 7일지난 회원 영구삭제"));
+        add(linkTo(ApiStoreController.class).slash("--").withRel("검색"));
         //TODO 검색 링크추가
     }
 }
